4a8c1ebf65f935b2b77bb64f8c817858748e9035
Anthony DI STEFANO <anthony.distefano@hotmail.fr>
2019-07-27 19:07:05 +0200

    Fix regex to find predicate & fix nullable value

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,14 +3,16 @@ import AutoMapper from "../core/AutoMapper";
 export const getKeyFromPredicate = <TSource>(
   predicate: (object: TSource) => any
 ): string => {
-  const key = /\.([^\.;]+);?\s*\}$/.exec(predicate.toString());
+  const key = /\.([^\.;]+);?\s*$/.exec(predicate.toString());
   return key && key.length !== 0 ? key[1] : "";
 };
 
 export const convert = (
-  data: any,
+  data: any = null,
   type: string | null = AutoMapper.TYPES.STRING
-) => {
+): any => {
+  if (!data) return null;
+
   switch (type) {
     default:
     case AutoMapper.TYPES.STRING:
