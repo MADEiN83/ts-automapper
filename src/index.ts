18d2adc244fe5c00e47b936cd2f0af2bdb0d2e15
MADEiN83 <MADEiN83@users.noreply.github.com>
2022-03-19 08:19:07 +0100

    fix: wrong imports (#28)
    * feat: adding build script in demo project in order to test the build process
    
    * fix: clear unused import
    
    * fix: wrong import
    
    * feat: clean config files
    
    * fix: update demo project dependencies
    
    * feat: release v1.2.1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import AutoMapper from "./core/automapper";
-import { AutoMapperTypes, AutoMapperOptions } from "core/interfaces";
+import { AutoMapperTypes, AutoMapperOptions } from "./core/interfaces";
 
 export default AutoMapper;
 export { AutoMapperTypes, AutoMapperOptions };
